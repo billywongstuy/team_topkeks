5e2127c2fa3fb7bb48b08d5f0416d71bacebe306
Patrick Chan <patrickchan1999@gmail.com>
2017-11-11 13:44:53 -0500

    added base for api.py

---
+++ app/api.py
@@ -0,0 +1,28 @@
+import flask
+import os
+import sys
+import werkzeug.contrib.fixers
+
+####  ALL OF THIS STUFF SHOULD REMAIN FREE FLOATING ####
+app = flask.Flask(__name__)
+
+app.wsgi_app = werkzeug.contrib.fixers.ProxyFix(app.wsgi_app)
+try:
+    app.secret_key = sys.argv[sys.argv.index('--key') + 1]
+except ValueError:
+    app.secret_key = 'e5fce5faa2e20b203c014f358f73c48f7129084ab1643c9fa6a0f87ff7a546a2'
+    #get value via os.urandom(32).encode('hex')
+
+@app.errorhandler(404)
+@utils.log_name
+def page_not_found(error):
+    return flask.render_template('404.html'), 404
+
+#### FREE FLOATING SECTION ENDS HERE ####
+
+def run():
+    app.debug = True
+    app.run(host = '0.0.0.0', port = 5000)
+
+if __name__ == '__main__':
+    run()
